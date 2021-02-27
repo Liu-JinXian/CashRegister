3b03fb34c656fa1ac287f2df1d1e6ebe83f1d0a1
Liu-JinXian <stevenliu860427@gmail.com>
2021-02-27 11:14:04 +0800

    Initial Commit

---
+++ cashRegister/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cashRegister
+//
+//  Created by 劉晉賢 on 2021/2/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
